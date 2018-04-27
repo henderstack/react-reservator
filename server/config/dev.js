0ceacdf5525e5cc90244ad3cb303a80780f91c6e
Bryan Henderson <bhenderson71@gmail.com>
2018-04-27 15:46:53 -0500

    Pruning

--- server/config/dev.js
+++
@@ -1,5 +0,0 @@
-// dev.js - DO NOT COMMIT THIS FILE
-module.exports = {
-    mongoURI: 'mongodb://devuser:ismedude@ds135747.mlab.com:35747/bh-react-reservator-dev',
-    cookieKey: 'poiulkjlikjillkiliertdkel'
-};
